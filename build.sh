704e02c2a2f71e3e9b1f6d2d6167d5038738dec5
peter <peter@retep.org>
2020-02-20 20:27:05 +0000

    Make boot script at build time

--- build.sh
+++ build.sh
@@ -9,7 +9,20 @@ echo " EQUS \"0.01 ($(date "+%d %b %Y"))\"" >version.asm
 # Copyright year
 echo " EQUS \"$(date "+%Y")\"" >copyright.asm
 
-rm -f $DISK $TMPDISK
+rm -f $DISK $TMPDISK boot
+
+# Create the !BOOT script to load the rom into sideways bank 4
+(
+  # The date here is so we know we are loading the correct image
+  # The way I copy an ssd to the gotek is via ssh to a
+  # Raspberry PI 0W which is masquerading as the USB stick
+  # and it doesn't always pick up the new image immediately
+  echo "REM Image build date"
+  echo "REM $(date)"
+  echo
+  echo "*SRLOAD BRAIL 8000 4"
+  echo "*ROMS"
+) >boot
 
 # Compile a local rom image, for programming into an actual EEPROM
 # -D FILLBANK=1 tells rom.asm to ensure the image is 16K
